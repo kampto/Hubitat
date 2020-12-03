2e6df62f0a3eb5f2a91e89c2ebf212b1bd65469b
kampto <31904505+kampto@users.noreply.github.com>
2020-12-02 16:48:43 -0800

    Update Child_Temperature_Sensor_Advanced.groovy

--- Drivers/Child_Temperature_Sensor_Advanced.groovy
+++ Drivers/Child_Temperature_Sensor_Advanced.groovy
@@ -35,9 +35,9 @@ metadata {
     
     preferences {
         input name: "tempOffset", type: "number", title: "<b>Temperature Offset</b>", description: "Offset temperature +/- Deg", range: "*...*", defaultValue: 0, required: false, displayDuringSetup: false
-		input name: "units", type: "enum", title: "<b>Temperature Unit Conversion</b>", description: "Default = none (DegF)", defaultValue: "1", required: false, multiple: false, options:[["1":"none"], ["2":"Fahrenheit to Celsius"], ["3":"Celsius to Fahrenheit"]], displayDuringSetup: false
+	input name: "units", type: "enum", title: "<b>Temperature Unit Conversion</b>", description: "Default = none (DegF)", defaultValue: "1", required: false, multiple: false, options:[["1":"none"], ["2":"Fahrenheit to Celsius"], ["3":"Celsius to Fahrenheit"]], displayDuringSetup: false
         input name: "logEnable", type: "bool", title: "<b>Enable debug logging?</b>", description: "Will Auto Disable in 30min", defaultValue: true
-		input name: "multiplier", type: "enum", title: "<b>Number Multiplier</b>", description: "Default = x1", defaultValue: "1", required: false, multiple: false, options:[["0.001":"x0.001"], ["0.01":"x0.01"], ["0.1":"x0.1"],["1":"x1"], ["10":"x10"], ["100":"x100"], ["1000":"x1000"]], displayDuringSetup: false
+	input name: "multiplier", type: "enum", title: "<b>Number Multiplier</b>", description: "Default = x1", defaultValue: "1", required: false, multiple: false, options:[["0.001":"x0.001"], ["0.01":"x0.01"], ["0.1":"x0.1"],["1":"x1"], ["10":"x10"], ["100":"x100"], ["1000":"x1000"]], displayDuringSetup: false
         input name: "numDecimalPlaces", type: "enum", title: "<b>Number of Decimals Places</b>", description: "Default = 1", defaultValue: "1", required: false, multiple: false, options:[["0":"0"], ["1":"1"], ["2":"2"], ["3":"3"]], displayDuringSetup: false
         input name: "lastUpdateEnable", type: "bool", title: "<b>Enable Last Update Attribute?</b>", defaultValue: true
         input name: "clockformat", type: "bool", title: "<b>Use 24 hour clock?</b>", description: "Used in Last Update if Enabled", defaultValue: true
@@ -50,7 +50,7 @@ metadata {
 
 def parse(String description) {
     if (logEnable) log.info "Raw capability parse (${description})"
-	def parts = description.split(" ")
+    def parts = description.split(" ")
     def name  = parts.length>0?parts[0].trim():null
     def value = parts.length>1?parts[1].trim():null
     def dispUnit
@@ -74,25 +74,37 @@ def parse(String description) {
         if (logEnable) log.debug "Sent Value = ${tmpValue} " + dispUnit
         
 //// Send Max & Min VALUE Conversion Calc and Reset, if Enabled
-        float tmpInputMinValue = inputMinValue as float
-        float tmpInputMaxValue = inputMaxValue as float  
-        float hour = new Date().format("HH", location.timeZone) as float 
-        float minute = new Date().format("mm", location.timeZone) as float
-        if (max_minResetEnable) {
-         if (hour == 00 && minute < 31) {   // 30min window to reset    
-            device.updateSetting("inputMinValue", [value: 500, type: "number"]) 
-            device.updateSetting("inputMaxValue", [value: -50, type: "number"]) }    // Reset Max Value after midnite
-            }
-        if (max_minEnable) {
-         if (tmpInputMaxValue < tmpValue) {device.updateSetting("inputMaxValue", [value: tmpValue, type: "number"]) 
+	if (max_minEnable) {
+		if (max_minResetEnable) {
+		 float tmpHour = new Date().format("HH", location.timeZone) as float 
+         float tmpMinute = new Date().format("mm", location.timeZone) as float	
+			 
+			if (tmpHour == 0 && tmpMinute < 31) {   // 30min window to reset    
+           		sendEvent(name: "maxValue", value: tmpValue, unit: " max")   // Send new Max Value if enabled
+           		device.updateSetting("inputMaxValue", [value: tmpValue, type: "number"])  
+				if (logEnable) {log.info "New Reset Max Value is ${tmpValue} " + dispUnit}
+             
+           		sendEvent(name: "minValue", value: tmpValue, unit: " min")   // Send new Min Value if enabled
+           		device.updateSetting("inputMinValue", [value: tmpValue, type: "number"]) 
+				if (logEnable) {log.info "New Reset Min Value is ${tmpValue} " + dispUnit}
+           	}
+		 }          
+        
+		 float tmpInputMinValue = inputMinValue as float
+         float tmpInputMaxValue = inputMaxValue as float 
+					
+		if (tmpInputMaxValue < tmpValue) { 
            sendEvent(name: "maxValue", value: tmpValue, unit: " max")   // Send new Max Value if enabled
-           if (logEnable) log.info "New Max Value is ${tmpValue} " + dispUnit
+           if (logEnable) {log.info "New Max Value is ${tmpValue} " + dispUnit}
+           device.updateSetting("inputMaxValue", [value: tmpValue, type: "number"])  
            }
-         if (tmpValue < tmpInputMinValue) {device.updateSetting("inputMinValue", [value: tmpValue, type: "number"]) 
+			
+		if (tmpValue < tmpInputMinValue) {
            sendEvent(name: "minValue", value: tmpValue, unit: " min")   // Send new Min Value if enabled
-           if (logEnable) log.info "New Min Value is ${tmpValue} " + dispUnit
+           if (logEnable) {log.info "New Min Value is ${tmpValue} " + dispUnit}
+           device.updateSetting("inputMinValue", [value: tmpValue, type: "number"]) 
            }   
-        }
+      }
         
 //// Send Last Update Time, if Enabled                
         def timeString = clockformat ? "HH:mm" : "h:mm: a" // 24Hr : 12Hr
