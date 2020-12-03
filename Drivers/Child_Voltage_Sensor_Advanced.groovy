08a1f68ef122e294a36e7666f7519a8477081ffc
kampto <31904505+kampto@users.noreply.github.com>
2020-12-02 16:50:55 -0800

    Update Child_Voltage_Sensor_Advanced.groovy

--- Drivers/Child_Voltage_Sensor_Advanced.groovy
+++ Drivers/Child_Voltage_Sensor_Advanced.groovy
@@ -25,8 +25,8 @@
 metadata {
 	definition (name: "Child Voltage Sensor Advanced", namespace: "kampto", author: "T. Kamp", 
         importUrl: "https://github.com/kampto/Hubitat/blob/main/Drivers/Child_Voltage_Sensor_Advanced.groovy") {
-		    capability "Voltage Measurement"
-		    capability "Sensor"
+	    capability "Voltage Measurement"
+	    capability "Sensor"
         
     	    attribute "lastUpdated", "String"    
             attribute "maxValue", "number"  
@@ -36,7 +36,7 @@ metadata {
     preferences {
         input name: "logEnable", type: "bool", title: "<b>Enable debug logging?</b>", description: "Will Auto Disable in 30min", defaultValue: true
         input name: "units", type: "enum", title: "<b>Voltage Units</b>", description: "Default = V", defaultValue: "2", required: false, multiple: false, options:[["1":"mV"], ["2":"V"], ["3":"KV"]], displayDuringSetup: false
-		input name: "multiplier", type: "enum", title: "<b>Number Multiplier</b>", description: "Default = x1", defaultValue: "1", required: false, multiple: false, options:[["0.001":"x0.001"], ["0.01":"x0.01"], ["0.1":"x0.1"],["1":"x1"], ["10":"x10"], ["100":"x100"], ["1000":"x1000"]], displayDuringSetup: false
+	input name: "multiplier", type: "enum", title: "<b>Number Multiplier</b>", description: "Default = x1", defaultValue: "1", required: false, multiple: false, options:[["0.001":"x0.001"], ["0.01":"x0.01"], ["0.1":"x0.1"],["1":"x1"], ["10":"x10"], ["100":"x100"], ["1000":"x1000"]], displayDuringSetup: false
         input name: "numDecimalPlaces", type: "enum", title: "<b>Number of Decimals Places</b>", description: "Default = 1", defaultValue: "1", required: false, multiple: false, options:[["0":"0"], ["1":"1"], ["2":"2"], ["3":"3"]], displayDuringSetup: false
         input name: "lastUpdateEnable", type: "bool", title: "<b>Enable Last Update Attribute?</b>", defaultValue: true
         input name: "clockformat", type: "bool", title: "<b>Use 24 hour clock?</b>", description: "Used in Last Update if Enabled", defaultValue: true
@@ -49,7 +49,7 @@ metadata {
 
 def parse(String description) {
     if (logEnable) log.info "Raw capability parse (${description})"
-	def parts = description.split(" ")
+    def parts = description.split(" ")
     def name  = parts.length>0?parts[0].trim():null
     def value = parts.length>1?parts[1].trim():null
     def dispUnit
@@ -69,26 +69,38 @@ def parse(String description) {
         if (logEnable) log.debug "Sent Value = ${tmpValue} " + dispUnit
         
 //// Send Max & Min VALUE Conversion Calc and Reset, if Enabled
-        float tmpInputMinValue = inputMinValue as float
-        float tmpInputMaxValue = inputMaxValue as float 
-        float hour = new Date().format("HH", location.timeZone) as float 
-        float minute = new Date().format("mm", location.timeZone) as float
-        if (max_minResetEnable) {
-         if (hour == 00 && minute < 31) {   // 30min window to reset
-            device.updateSetting("inputMinValue", [value: 50000, type: "number"]) 
-            device.updateSetting("inputMaxValue", [value: 0, type: "number"]) }    // Reset Max Value after midnite
-            }
-        if (max_minEnable) {
-         if (tmpInputMaxValue < tmpValue) {device.updateSetting("inputMaxValue", [value: tmpValue, type: "number"]) 
-           sendEvent(name: "maxValue", value: tmpValue, unit: " max")   // Send new Max Value if enabled
-           if (logEnable) log.info "New Max Value is ${tmpValue} " + dispUnit
-           }
-         if (tmpValue < tmpInputMinValue) {device.updateSetting("inputMinValue", [value: tmpValue, type: "number"]) 
-           sendEvent(name: "minValue", value: tmpValue, unit: " min")   // Send new Min Value if enabled
-           if (logEnable) log.info "New Min Value is ${tmpValue} " + dispUnit
-           }   
-        }
+	if (max_minEnable) {
+		if (max_minResetEnable) {
+		 float tmpHour = new Date().format("HH", location.timeZone) as float 
+         float tmpMinute = new Date().format("mm", location.timeZone) as float	
+			 
+			if (tmpHour == 0 && tmpMinute < 31) {   // 30min window to reset    
+           		sendEvent(name: "maxValue", value: tmpValue, unit: " max")   // Send new Max Value if enabled
+           		device.updateSetting("inputMaxValue", [value: tmpValue, type: "number"])  
+				if (logEnable) {log.info "New Reset Max Value is ${tmpValue} " + dispUnit}
+             
+           		sendEvent(name: "minValue", value: tmpValue, unit: " min")   // Send new Min Value if enabled
+           		device.updateSetting("inputMinValue", [value: tmpValue, type: "number"]) 
+				if (logEnable) {log.info "New Reset Min Value is ${tmpValue} " + dispUnit}
+           		}
+		 }          
         
+	 float tmpInputMinValue = inputMinValue as float
+         float tmpInputMaxValue = inputMaxValue as float 
+					
+		if (tmpInputMaxValue < tmpValue) { 
+           sendEvent(name: "maxValue", value: tmpValue, unit: " max")   // Send new Max Value if enabled
+           if (logEnable) {log.info "New Max Value is ${tmpValue} " + dispUnit}
+           device.updateSetting("inputMaxValue", [value: tmpValue, type: "number"])  
+           }
+			
+		if (tmpValue < tmpInputMinValue) {
+           sendEvent(name: "minValue", value: tmpValue, unit: " min")   // Send new Min Value if enabled
+           if (logEnable) {log.info "New Min Value is ${tmpValue} " + dispUnit}
+           device.updateSetting("inputMinValue", [value: tmpValue, type: "number"]) 
+           }   
+      }
+       
 //// Send Last Update Time, if Enabled                 
         def timeString = clockformat ? "HH:mm" : "h:mm: a" // 24Hr : 12Hr
         def nowDay = new Date().format("MMM dd", location.timeZone)
